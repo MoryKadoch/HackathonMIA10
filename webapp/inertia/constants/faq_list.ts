7047a9598d40d738a7148e5724187257cf24bfcf
Sonny <sonnyasdev@gmail.com>
2024-05-31 13:46:48 +0200

    feat: change responses of faq

--- webapp/inertia/constants/faq_list.ts
+++ webapp/inertia/constants/faq_list.ts
@@ -1,55 +1,72 @@
 type FAQItem = { header: string; content: string };
 
-export const faqList: FAQItem[] = [
+export const FAQ_LIST: FAQItem[] = [
+  {
+    header:
+      'La France a organisé 6 JO : 3 d’hiver et 3 d’été (en comptant celui de 2024) ?',
+    content:
+      "Faux, la France a accueilli 2 Jeux Olympiques d'été et 3 Jeux Olympiques d'hiver",
+  },
   {
     header:
       'La France est le 2ème pays qui a organisé le plus de JO après les USA (8 JO) ?',
-    content: 'Vrai',
+    content:
+      "C'est vrai, la France est le deuxième pays ayant organisé le plus de Jeux Olympiques après les États-Unis.",
   },
   {
-    header: 'Les JO d’hiver sont nés à Chamonix en 1924 ?',
-    content: 'Vrai',
+    header: "Les JO d'hiver sont nés à Chamonix en 1924 ?",
+    content:
+      "Exact, les Jeux Olympiques d'hiver ont vu le jour à Chamonix en 1924",
   },
   {
     header: 'JO de Paris, en 1900 : les femmes peuvent participer aux JO ?',
-    content: 'Oui',
+    content:
+      "C'est vrai, les femmes ont pu participer aux Jeux Olympiques de Paris en 1900.",
   },
   {
     header:
       'Seuls 4 athlètes ont remporté des médailles à la fois aux JO d’hiver et d’été. Une seule d’entre eux, Christa Ludinger-Rothenburger, a remporté des médailles au cours de la même année ?',
-    content: 'Vrai',
+    content:
+      'Vrai ! Christa Ludinger-Rothenburger a remporté des médailles au cours de la même année.',
   },
   {
     header:
       'De 1924 à 1992, les JO d’hiver et d’été avaient lieu au cours de la même année. Désormais, ils sont organisés selon des cycles distincts avec une alternance de 2 ans ?',
     content:
-      'Vrai, on voit qu’il y a bien 2 occurrences de pays accueillant les JO par an de 1924 à 1992 et 1 occurrence par an de 1994 à 2022',
+      "C'est vrai, on constate qu'il y a eu deux occurrences de pays accueillant les Jeux Olympiques par an de 1924 à 1992, puis une occurrence par an de 1994 à 2022.",
   },
   {
     header:
       'Tarzan lui-même a participé aux JO. En effet, Johnny Weissmuller, ancien athlète devenu acteur et ayant interprété 12 films de Tarzan, a remporté 5 médailles d’or en natation dans les années 1920 ?',
-    content: 'Faux, il n’a remporté que 3 médailles',
+    content: "C'est faux, Tarzan n'a remporté que 3 médailles.",
   },
   {
     header:
       'Les JO de Londres de 2012 ont été les 1ers JO durant lesquels tous les pays participants ont envoyé des athlètes de sexe féminin',
     content:
-      'Faux la liste des pays qui n’ont pas envoyé de femmes sont Botswana, Afghanistan, Georgia, Saudi Arabia, Uzbekistan, Dominican Republic, Grenada, Estonia, Morocco, Cyprus, Puerto Rico, Bahamas, Armenia, Qatar, Portugal, Kuwait, Latvia, Venezuela, Islamic Republic of Iran, Algeria, Uganda, Guatemala, Gabon, Trinidad and Tobago',
+      "C'est faux, la liste des pays qui n'ont pas envoyé de femmes aux Jeux Olympiques inclut le Botswana, l'Afghanistan, la Géorgie, l'Arabie Saoudite, l'Ouzbékistan, la République Dominicaine, la Grenade, l'Estonie, le Maroc, Chypre, Porto Rico, les Bahamas, l'Arménie, le Qatar, le Portugal, le Koweït, la Lettonie, le Venezuela, la République Islamique d'Iran, l'Algérie, l'Ouganda, le Guatemala, le Gabon, ainsi que Trinité-et-Tobago.",
+  },
+  {
+    header:
+      'Les Jeux de 2016, à Rio, marqueront la 1è manifestation des JO en Amérique du Sud ?',
+    content:
+      "C'est vrai, les Jeux de 2016 à Rio ont marqué la première fois que les Jeux Olympiques se tenaient en Amérique du Sud.",
   },
   {
     header:
       'Combien de médailles la France a remporté : en tout, en Or, en argent et en Bronze (depuis le début des JO) ?',
     content:
-      "Médailles remportées par la France : 952, médailles d'or remportées : 282, médailles d'argent : 305, médailles de bronze : 365",
+      'La France a gagné un total de 952 médailles lors de compétitions. Parmi celles-ci, 282 sont en or, 305 en argent, et 365 en bronze.',
   },
   {
     header: 'Lors de quelle JO la France a eu le plus et le moins de succès ?',
     content:
-      'En 1900 la France a eu le plus de succès. En 1904 la France a eu le moins de succès.',
+      "En 1900, la France a connu son plus grand succès, tandis qu'en 1904, elle a enregistré ses performances les moins impressionnantes.",
   },
   {
     header:
       'Peut-on considérer que la France est la grande spécialiste de certaine(s) discipline(s) sportive(s), comparativement aux autres notions ? Laquelle / Lesquelles ?',
-    content: 'Oui en Athletics',
+    content:
+      "Oui, la France est souvent considérée comme une spécialiste dans certaines disciplines sportives, notamment l'athlétisme.",
   },
 ];
