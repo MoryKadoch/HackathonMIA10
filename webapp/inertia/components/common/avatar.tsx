d31c5de7b0b38b8dd8ed32c34b039a733e484945
Sonny <sonnyasdev@gmail.com>
2024-05-31 13:46:30 +0200

    feat: update team member avatars

--- webapp/inertia/components/common/avatar.tsx
+++ webapp/inertia/components/common/avatar.tsx
@@ -3,6 +3,7 @@ import styled from '@emotion/styled';
 const Avatar = styled.img<{ size: number }>(({ size }) => ({
   height: `${size}px`,
   width: `${size}px`,
+  borderRadius: '50%',
 }));
 
 export default Avatar;
